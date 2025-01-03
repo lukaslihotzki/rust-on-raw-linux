e7426bc06d95303ddc8dec31bf2860d5ba15d0cc
Lukas Lihotzki <lukas@lihotzki.de>
2025-01-03 03:45:49 +0100

    fix: build without sstrip

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 ARCH=${ARCH:-$(rustc -vV | sed -n 's/-.*//;s/^host: //p')}
 
 if rustc -vV | grep -q nightly; then
-    cargo build -p utilities  --features experimental --target "$ARCH"-unknown-none --release -Z build-std=core,panic_abort -Z build-std-features=panic_immediate_abort
+    cargo build -p utilities --features experimental --target "$ARCH"-unknown-none --release -Z build-std=core,panic_abort -Z build-std-features=panic_immediate_abort
 else
     cargo build -p utilities --target "$ARCH"-unknown-none --release
 fi
 
-sstrip ./target/aarch64-unknown-none/release/*
+if command -v sstrip >/dev/null; then
+    set -- cat echo false sleep true
+    mkdir -p target/"$ARCH"-unknown-none/sstrip
+    cp ${@/#/target/"$ARCH"-unknown-none/release/} target/"$ARCH"-unknown-none/sstrip
+    sstrip ${@/#/target/"$ARCH"-unknown-none/sstrip/}
+fi
